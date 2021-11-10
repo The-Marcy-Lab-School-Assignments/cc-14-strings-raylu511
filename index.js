69591b5a8d2b07b9cc489a1f98ec8383b41cdaaa
Ray Lu <raylu511@gmail.com>
2021-11-10 15:12:46 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //write your code here
+function stripUpperCase(string) {
+    let lowerString = '';
+    const range = 'AZ';
+    const asciiOfA = range.charCodeAt(0);
+    const asciiOfZ = range.charCodeAt(1);
+    for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) > asciiOfZ || string.charCodeAt(i) < asciiOfA) {
+            lowerString += string[i];
+        }
+    }
+         // Create newString
+         // Find ascii code of A to Z
+         // Iterate through string
+         // If char is not between the range
+            // Concat to new string
+    return lowerString;
+}
+
+console.log(stripUpperCase('Hello!'));              // 'ello!'
+console.log(stripUpperCase('SevenEleven'));         // 'evenleven'
+console.log(stripUpperCase("Don't play with Me!")); // 'ont play with e!'
